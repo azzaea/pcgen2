02ce753bf105c9be8ff3f26bf2f1358c7fb55643
azzaea <azzaea@uofk.edu>
2020-08-19 17:50:01 +0200

    format pcgenTest code for readibility

--- R/pcgenTest.R
+++ R/pcgenTest.R
@@ -40,9 +40,9 @@
 #' @export
 #'
 pcgenTest <- function(x, y, S, suffStat, covariates = NULL, QTLs = integer(), K = NULL,
-                          alpha = 0.01, max.iter = 50, stop.if.significant = TRUE,
-                          use.res = FALSE, res.cor = NULL) {
-
+                      alpha = 0.01, max.iter = 50, stop.if.significant = TRUE,
+                      use.res = FALSE, res.cor = NULL) {
+  
   # suffStat = d; x = 2; y = 8; S = c(1,11)
   # QTLs=integer(); covariates=NULL;
   # skip.nongenetic=1; genVar=rep(TRUE, ncol(suffStat));
@@ -51,17 +51,17 @@ pcgenTest <- function(x, y, S, suffStat, covariates = NULL, QTLs = integer(), K 
   # mean.adj = 'none'; Vg = NULL; Ve = NULL; dec = NULL;
   # max.iter = 50; stop.if.significant = TRUE;
   # return.cond.mean = FALSE
-
+  
   #suffStat = dr[, c(1, 26:36)]; covariates = dr[, 37:38]; x = 6; y = 7;
   #S = 8:9; QTLs=integer(); covariates=NULL; alpha = 0.01; mean.adj = 'none'; 
   #Vg = NULL; Ve = NULL; dec = NULL; max.iter = 50;stop.if.significant = TRUE; 
   #return.cond.mean = FALSE
-
-  #### First, some checking...
-
+  
+  ## First, some checking ------------------------------------------------
+  
   if (class(QTLs)!='integer') {stop('QTLs should be a vector of integers')}
   if (1 %in% QTLs) {stop('QTLs should not contain the genotype column (G)')}
-
+  
   # stop if the first column in suffStat does not have the name G
   stopifnot(names(suffStat)[1] == 'G')
   # stop if the same variables occurs more than once
@@ -73,87 +73,65 @@ pcgenTest <- function(x, y, S, suffStat, covariates = NULL, QTLs = integer(), K 
   # Neither we test cond. indep. of a QTL and genotype
   if (x %in% QTLs & y==1) {stop()}
   if (y %in% QTLs & x==1) {stop()}
-
+  
   # put the QTL column numbers in ascending order
   QTLs <- sort(QTLs)
-
+  
   ###################################################################
   # Define a data.frame X, containing the covariates. It is also defined when
   # there are no covariates, in which case it will have zero columns.
-
+  
   if (is.null(covariates)) {
     X <- as.data.frame(matrix(0,nrow(suffStat),0))
   } else {
     X <- as.data.frame(covariates)
     names(X) <- paste0('covariate_', 1:ncol(covariates))
   }
-
-  ######################################
-
-
-  ### Case 1: ------------------------------------------------------------
+  
+  ##########################################################################
+  
+  
+  ## Case 1: Trait \perp Trait | {Traits + QTLs} ---------------------------
   # x and y both represent real traits (no QTLs), and 1
-  # (direct genetic effects) is not in S. S may contain QTLs
-
+  # (direct genetic effects) is not in S. S may contain QTLs 
+  
   if (!(1 %in% c(x,y,S)) & !(any(c(x,y) %in% QTLs))) {S <- c(1, S)}
-
-  ###################################################################
+  
+  #########################################################################
+  
+  ## Case 2: Trait \perp G | {Traits + QTLs} ------------------------------
   # The case where one of x and y is the number 1 (direct genetic effects).
-  # S may also contain QTLs
-
+  # S may also contain QTLs 
+  
   if (x==1 | y==1) {
-
     if (x==1) {
-
-      if (length(S)==0) {
-
-        return(gen.var.test(y=suffStat[,y],X=X,G=suffStat[,1]))
-
-      } else {
-
+      if (length(S)!=0) 
         X <- as.data.frame(cbind(X, suffStat[,S]))
-
-        return(gen.var.test(y=suffStat[,y], X = X, G=suffStat[,1]))
-
-      }
+      return(gen.var.test(y = suffStat[,y], X = X, G = suffStat[,1]))
     }
-
     if (y==1) {
-
-      if (length(S)==0) {
-
-        return(gen.var.test(y=suffStat[,x],X=X,G=suffStat[,1]))
-
-      } else {
-
+      if (length(S)!=0) 
         X <- as.data.frame(cbind(X, suffStat[,S]))
-
-        return(gen.var.test(y=suffStat[,x], X = X, G=suffStat[,1]))
-
-      }
-
+      return(gen.var.test(y = suffStat[,x], X = X, G=suffStat[,1]))
     }
-
   }
-
+  
   ########################################################################
+  
+  ## Case 3: Trait \perp QTL | {Traits + G + QTL} -----------------------
   # The case where one of x and y is a QTL;
   # the number 1 (direct genetic effects) may or may not be in S.
   # S may also contain additional QTLs
-
+  
   if (any(QTLs %in% c(x,y))) {
-
-    if (y %in% QTLs) {
-      # In this case, make sure that x is the QTL, not y (just to simplify the code)
+    if (y %in% QTLs) { # make x the QTL, not y (just to simplify the code)
       temp.x <- x
       x <- y
       y <- temp.x
     }
-
     sns  <- names(suffStat)
-
+    
     if (1 %in% S) {
-
       # Because of the 'hierarchy' (G would 'eat up' all QTL variance), drop 1 from S 
       # (in this case only)
       if (is.null(covariates)) {
@@ -165,9 +143,7 @@ pcgenTest <- function(x, y, S, suffStat, covariates = NULL, QTLs = integer(), K 
                                             '+', paste(sns[c(setdiff(S,1), x)], collapse='+'))),
                            data = cbind(suffStat, X)))$coefficients[ncol(X) + length(S) + 1, 4]
       }
-
-    } else {
-
+    } else { # G is not in S
       if (is.null(covariates)) {
         pval <- summary(lm(as.formula(paste(sns[y], '~', paste(sns[c(S,x)], collapse='+'))), 
                            data = suffStat))$coefficients[length(S)+2,4]
@@ -176,45 +152,33 @@ pcgenTest <- function(x, y, S, suffStat, covariates = NULL, QTLs = integer(), K 
                                             '+', paste(sns[c(S,x)], collapse = '+'))),
                            data = cbind(suffStat, X)))$coefficients[ncol(X) + length(S)+2,4]
       }
-
     }
-
+    
     return(pval)
   }
-
+  
   ###################################################################
+  ## Case 1*: Trait \perp Trait | {G, QTL and Traits} ----------------------
   # The case where x and y both represent real traits (no QTLs), and 1
   # (direct genetic effects: G) IS contained in S. S may also contain QTLs.
   # We do not skip any tests here.
   # Note that G will account for QTLs that may not be contained in S.
-
-
-    #if (1 %in% S) {  # redundant; at this point, 1 MUST be contained in S
-
+  
+  
   if (use.res == TRUE) {
-
     out <- gaussCItest(x = x - 1, y = y - 1, S = (setdiff(S, 1) - 1),
                        suffStat = list(C = res.cor, n = nrow(suffStat)))
-
-  } else {
-
+  } else { # No residuals
     if (length(S)==1) {
-
-      out <- res.covar.test(x=suffStat[,x],y=suffStat[,y], X=X,
-                            G=suffStat[,1], alpha = alpha)
-
+      out <- res.covar.test(x = suffStat[,x], y=suffStat[,y], X = X,
+                            G = suffStat[,1], alpha = alpha)
     } else {
-
       X <- as.data.frame(cbind(X,suffStat[,setdiff(S,1)]))
-
       out <- res.covar.test(x = suffStat[,x], y = suffStat[,y],
                             X = X, G = suffStat[,1], alpha = alpha,
-                            max.iter = max.iter,
-                            stop.if.significant = stop.if.significant)
+                            max.iter = max.iter, stop.if.significant = stop.if.significant)
     }
-
   }
   return(out[1])
-
 }
 
