1f9a09863cc8db9fdb4078f5161a447e5a41f928
azzaea <azzaea@uofk.edu>
2020-08-20 10:43:29 +0200

    make gen.var.test suitable for different K

--- R/gen.var.test.R
+++ R/gen.var.test.R
@@ -1,26 +1,40 @@
 gen.var.test <- function(y, X = data.frame(), G, K = NULL, return.fitted = FALSE) {
   # y=d[,2]; X=d[,3]; G=d[,1] # previous set up
   # y = dm[, 2]; X = dm[,3]; G=dm[,1]; K = K # new contribution
-
-  X <- as.matrix(X)
-  if (ncol(X) == 0) { # Y_S = \phi
-    X <-  matrix(1, nrow = length(y))
-  } else {  # Y_S \ne \phi    
-    X <- cbind(1, X)
+  
+  if (is.null(K)) { # Old code: suitable for plant data with replicates (K = ZZ^t)
+    X <- as.data.frame(X)
+    if (ncol(X) > 0) {
+      a <- data.frame(y = y, X, G = G)
+      names(a) <- c('y',paste0('C',1:ncol(X)),'G')
+    } else {
+      a <- data.frame(y = y, G = G)
+    }
+    a <- a[!is.na(a$y),]
+    G <- factor(as.character(G))
+    lm.obj <- lm(as.formula(paste("y~", paste(names(a)[-1],collapse = "+"))), data=a)
+    av <- anova(lm.obj)
+    pvalue <- av[[5]][1+ncol(X)]
+    if (return.fitted == TRUE) 
+      fitted.values <- as.numeric(as.matrix(X) %*% matrix(as.numeric(coefficients(lm.obj))[2:(ncol(X)+1)]))
+  } else {  # Generic K = A
+    X <- as.matrix(X)
+    if (ncol(X) == 0) { # Y_S = \phi
+      X <-  matrix(1, nrow = length(y))
+    } else {  # Y_S \ne \phi
+      X <- cbind(1, X)
+    }
+    lm.obj <- lmm.aireml(Y = y , X = X, K = K, verbose = F)
+    lrt <- 2*(lm.obj$logL - lm.obj$logL0) 
+    pvalue <- pchisq(lrt, df = 1, lower.tail = F)
+    if (return.fitted == TRUE)
+      fitted.values <- NULL #Not sure how to calculate it here
   }
   
-  lm.obj <- lmm.aireml(Y = y , X = X, K = K, verbose = F)
-  
-  lrt <- 2*(lm.obj$logL - lm.obj$logL0) # lrt statistic
-  pvalue <- pchisq(lrt, df = 1, lower.tail = F) 
-  
-  if (return.fitted == TRUE) {
-    # fitted.values <- as.numeric(   #I don't know how to produce this from gaston output
-    #   as.matrix(X) %*% matrix(as.numeric(coefficients(lm.obj))[2:(ncol(X)+1)]))
-    fitted.values <- NULL
+  if (return.fitted == TRUE) 
     return(list(pvalue, fitted.values))
-  } else {
+  else 
     return(pvalue)
-  }
+  
   
 }
