1dc563d20f6e8b2efa41085e65e7cb5c2f54b4ae
azzaea <azzaea@uofk.edu>
2020-12-10 22:38:03 +0100

    add tests files and draft vign

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(pcgen2)
+
+test_check("pcgen2")
---
+++ tests/testthat/test-skeleton2.R
@@ -0,0 +1,6 @@
+test_that("skeleton2() works", {
+  expect_equal(2 * 2, 4)
+  skip("skipping tests")
+  expect_equal(1, 1)
+  expect_equal(1, 1.000000001)
+})
